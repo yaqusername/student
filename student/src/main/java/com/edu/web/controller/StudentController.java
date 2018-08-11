601f351a4507cbddd1a8696e4c7b11ca39efa272
yaqusername <407139407@qq.com>
2018-08-11 10:26:00 +0800

    实现service层，serviceImpl层，controller层 添加资源文件

--- student/src/main/java/com/edu/web/controller/StudentController.java
+++ student/src/main/java/com/edu/web/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.edu.web.controller;
 
+import com.edu.domain.Student;
 import com.edu.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @Author Create By 407139407
@@ -13,11 +18,40 @@ import org.springframework.stereotype.Controller;
 public class StudentController {
     @Autowired
     private StudentService studentService;
-    //
+    //添加
+    @RequestMapping("/add.do")
+     String save(Student student){
+        if (studentService.save(student)){
+            System.out.print("添加成功");
+        }else{
+            System.out.print("添加失败");
+        }return "index";
+    }
 
-    //
+    //删除
+    @RequestMapping("/del.do")
+    public String delete(int id){
+        if(studentService.delete(id)){
+            System.out.print("OK");
+        }else{
+            System.out.print("Error");
+        }return null;
+    }
 
-    //
+    //修改
+    @RequestMapping("/update.do")
+    public String update(Student student){
+        if(studentService.update(student)){
+            System.out.print("ok");
+        }else{
+            System.out.print("Error");
+        }return "index";
+    }
 
-    //
+    //查询列表
+    @RequestMapping("/list.do")
+    @ResponseBody
+    public List<Student> queryAll(){
+        return studentService.queryAll();
+    }
 }
